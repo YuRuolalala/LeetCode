17d898ed4189856c43d3cb65a10a0e91a3a71516
YuRuolalala <2386402407@qq.com>
2022-09-23 18:25:44 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/Array/BinarySearch/Binary_Search_704.java

--- src/Hash/Valid_Anagram_242.java
+++ src/Hash/Valid_Anagram_242.java
@@ -6,17 +6,15 @@ import java.util.Arrays;
 
 public class Valid_Anagram_242 {
     public boolean isAnagram(String s, String t) {
-
-        int [] record=new int[26];
+        int[] record=new int[26];
         for(int i=0;i<s.length();i++){
             record[s.charAt(i)-'a']++;
         }
         for(int i=0;i<t.length();i++){
             record[t.charAt(i)-'a']--;
         }
-
-        for(int count:record){
-            if(count!=0){
+        for(int i=0;i<26;i++){
+            if(record[i]!=0){
                 return false;
             }
         }
